5a39ab10fc24386ae83de47cb7d2cc9ceff041fe
Abdullahi Garba <garbaabdullahi344@gmail.com>
2026-04-24 01:22:19 +0100

    Aggressive memory optimization for Puppeteer

--- services/whatsapp.js
+++ services/whatsapp.js
@@ -9,7 +9,18 @@ import { getCryptoPrice } from './cryptoApi.js';
 
 export const whatsappClient = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+    puppeteer: { 
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage', // CRUCIAL: Prevents RAM crashes in cloud environments
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // Forces Chrome to use less memory
+            '--disable-gpu'
+        ] 
+    }
 });
 
 whatsappClient.on('qr', (qr) => {
